93f483f18eda194515e8524453829adce88bd74c
Stack <stackingear@gmail.com>
2024-08-18 12:48:26 -0600

    Updated all classes for DT

--- Avarice/StaticData/Data.cs
+++ Avarice/StaticData/Data.cs
@@ -1,36 +1,35 @@
-﻿using Lumina.Excel.GeneratedSheets;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Avarice.StaticData
+﻿namespace Avarice.StaticData
 {
     internal class Data
     {
         public static readonly SortedList<ActionID, EnemyPositional> ActionPositional = new()
         {
+            //Monk
+            {ActionID.Demolish, EnemyPositional.Rear },
+            {ActionID.SnapPunch, EnemyPositional.Flank },
+            {ActionID.PouncingCoeurl, EnemyPositional.Flank },
+
             //Dragoon
             {ActionID.FangandClaw, EnemyPositional.Flank},
             {ActionID.WheelingThrust, EnemyPositional.Rear},
             {ActionID.ChaosThrust, EnemyPositional.Rear },
             {ActionID.ChaoticSpring, EnemyPositional.Rear },
-            //Monk
-            {ActionID.Demolish, EnemyPositional.Rear },
-            {ActionID.SnapPunch, EnemyPositional.Flank },
+
             //Ninja
             {ActionID.TrickAttack, EnemyPositional.Rear },
             {ActionID.AeolianEdge,EnemyPositional.Rear },
             {ActionID.ArmorCrush, EnemyPositional.Flank },
-            //Reaper
-            {ActionID.Gibbet, EnemyPositional.Flank},
-            {ActionID.Gallows, EnemyPositional.Rear },
-            {ActionID.ExecutionersGibbet, EnemyPositional.Flank},
-            {ActionID.ExecutionersGallows, EnemyPositional.Rear },
+
             //Samurai
             {ActionID.Gekko, EnemyPositional.Rear},
             {ActionID.Kasha, EnemyPositional.Flank },
+
+            //Reaper
+            {ActionID.Gibbet, EnemyPositional.Flank},
+            {ActionID.Gallows, EnemyPositional.Rear },
+            {ActionID.ExecGibbet, EnemyPositional.Flank},
+            {ActionID.ExecGallows, EnemyPositional.Rear },
+
             //Viper
             {ActionID.FlankstingStrike, EnemyPositional.Flank },
             {ActionID.FlanksbaneFang, EnemyPositional.Flank },
--- Avarice/Structs/PositionalState.cs
+++ Avarice/Structs/PositionalState.cs
@@ -6,4 +6,3 @@ public enum PositionalState
     Success,
     Failure
 }
-
